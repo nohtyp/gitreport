2497cc4163deea9b43800d38dd864a52fa062cca
nohtyp <thomas.foster80@gmail.com>
2014-08-19 14:39:36 -0400

    modified script to give how many commits for repository

--- gitreport.rb
+++ gitreport.rb
@@ -1,14 +1,24 @@
 #!/usr/bin/env ruby
 require 'curl'
 require 'curb'
+require 'json'
 
-url = ["https://api.github.com/repos/nohtyp/linode"]
+url = ["https://api.github.com/repos/nohtyp/linode/commits"]
+
+c=0
 
 gitobj = Curl::Easy.new
 
 url.each do |url|
-	gitobj.headers["User-Agent"] = "mytest-0.0"
+	gitobj.headers["User-Agent"] = "gitreport-0.0"
 	gitobj.url = url
 	gitobj.perform
-	puts "#{gitobj.body_str}"
+	@test = JSON.parse(gitobj.body_str)
 end
+
+@test.each do |x| 
+  puts x["comments_url"]
+  c += 1
+end
+
+puts "There are #{c} commits"
